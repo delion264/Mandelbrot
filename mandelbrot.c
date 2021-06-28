327233bc253129b3f9c27bfc652ead03834c1ac3
DONNY LEONE <leone.donny@gmail.com>
2021-06-28 12:42:33 +1000

    Base case for treating a single complex coordinate

--- mandelbrot.c
+++ mandelbrot.c
@@ -5,3 +5,28 @@ This program produces an image of the Mandelbrot set */
 #include <stdlib.h>
 #include <math.h>
 #include <stdint.h>
+
+#define MAX_ITERATIONS 128 /* Will be 65535 in final version */
+
+int main(int argc, char *argv[])
+{
+
+    double x, y; /* Complex coordinates of z_0 */
+    double u, v; /* Complex coordinates of z_k */
+    int k;       /* Iteration counter */
+
+    for (k = 0; k < MAX_ITERATIONS; k++)
+    {
+        double z2_re = u * u - v * v;
+        double z2_im = 2 * u * v;
+        double mag = sqrt(pow(z2_re, 2) + pow(z2_im, 2));
+
+        u = z2_re;
+        v = z2_im;
+    }
+
+    if (mag < 2)
+    {
+        printf("*");
+    }
+}
