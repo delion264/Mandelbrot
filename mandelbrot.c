aac5b4ef69c2bbf4202ee67f75cf0513d9e8cd95
DONNY LEONE <leone.donny@gmail.com>
2021-06-28 19:48:19 +1000

    Wrote function to calculate 'escape time' of a coordinate and loops in main() for iterating through pixels

--- mandelbrot.c
+++ mandelbrot.c
@@ -7,26 +7,58 @@ This program produces an image of the Mandelbrot set */
 #include <stdint.h>
 
 #define MAX_ITERATIONS 128 /* Will be 65535 in final version */
+#define WIDTH 600          /* Image width in pixels */
+#define HEIGHT 400         /* Image height in pixels */
+#define XMIN -1
+#define XMAX 1
+#define YMIN -1.5
+#define YMAX 1.5
 
 int main(int argc, char *argv[])
 {
+    int i, j;    /* Iterate over pixels */
+    double a, b; /* Complex coordinates that correspond to pixel divisions */
+    double xInterval = (XMAX - XMIN) / WIDTH;
+    double yInterval = (YMAX - YMIN) / HEIGHT;
 
-    double x, y; /* Complex coordinates of z_0 */
-    double u, v; /* Complex coordinates of z_k */
-    int k;       /* Iteration counter */
-
-    for (k = 0; k < MAX_ITERATIONS; k++)
+    for (i = 0; i < WIDTH / xInterval; i++)
     {
-        double z2_re = u * u - v * v;
-        double z2_im = 2 * u * v;
-        double mag = sqrt(pow(z2_re, 2) + pow(z2_im, 2));
+        for (j = 0; j < HEIGHT / yInterval; j++)
+        {
+            int s = mandelbrot(XMIN + i * xInterval, YMIN + j * yInterval);
+            if (s = MAX_ITERATIONS)
+            {
+                printf("*");
+            }
+        }
+    }
 
+    return EXIT_SUCCESS;
+}
+
+/* Escape time algorithm */
+int mandelbrot(double x, double y)
+{
+    double u = x; /* Complex coordinates of z_k */
+    double v = y;
+    double z2_re;
+    double z2_im;
+    double mag = 0; /* Magnitude of vector in complex space */
+    int k = 0;      /* Iteration counter */
+
+    while (k < MAX_ITERATIONS && mag < 2)
+    {
+        z2_re = u * u - v * v;
+        z2_im = 2 * u * v;
+        mag = sqrt(pow(z2_re, 2) + pow(z2_im, 2));
+
+        if (z2_re == x && z2_im == y)
+        {
+            break;
+        }
         u = z2_re;
         v = z2_im;
+        k++;
     }
-
-    if (mag < 2)
-    {
-        printf("*");
-    }
+    return k;
 }
