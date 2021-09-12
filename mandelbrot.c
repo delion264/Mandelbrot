80398c58c7b2ef076f8d5a5e2b7873424b67ae3b
DONNY LEONE <leone.donny@gmail.com>
2021-09-12 20:24:09 +1000

    Fix escape mandelbrot() function

--- mandelbrot.c
+++ mandelbrot.c
@@ -6,58 +6,46 @@ This program produces an image of the Mandelbrot set */
 #include <math.h>
 #include <stdint.h>
 
-#define MAX_ITERATIONS 65535 /* Will be 65535 in final version */
-#define WIDTH 150            /* Image width in pixels */
-#define HEIGHT 60            /* Image height in pixels */
+#define MAX_ITERATIONS 32 /* Will be 65535 in final version */
+#define WIDTH 160         /* Image width in pixels */
+#define HEIGHT 40         /* Image height in pixels */
 #define XMIN -1
 #define XMAX 1
 #define YMIN -1.5
 #define YMAX 1.5
 
-/* Escape time algorithm */
-int mandelbrot(double x, double y)
+/*  Escape time algorithm
+    Carries out the iteration z_k+1 = (z_k)^2 + c
+    u and v are Re(z_k) and Im(z_k) respectively
+    x and y are Re(c) and Im(c) respectively */
+int mandelbrot(double u, double v, double x, double y, int k)
 {
-    double u = x; /* Complex coordinates of z_k */
-    double v = y;
-    double z2_re;
-    double z2_im;
-    double mag = 0; /* Magnitude of vector in complex space */
-    int k = 0;      /* Iteration counter */
+    double z2_re = u * u - v * v; /* Calculate Re((z_k)^2) and Im((z_k)^2) */
+    double z2_im = 2 * u * v;
+    double fz_re = z2_re + x; /* Calculate Re(z_k+1) and Im(z_k+1) */
+    double fz_im = z2_im + y;
+    double mag = fz_re + fz_im; /* Magnitude squared of z_k+1 */
+    int i = 0;
 
-    while (k < MAX_ITERATIONS && mag < 4)
+    while (mag <= 2)
     {
-        z2_re = u * u - v * v;
-        z2_im = 2 * u * v;
-        mag = z2_re * z2_re + z2_im * z2_im;
-
-        if (z2_re == x && z2_im == y)
-        {
-            break;
-        }
         u = z2_re;
         v = z2_im;
-        k++;
+        z2_re = u * u - v * v;
+        z2_im = 2 * u * v;
+        fz_re = z2_re + x;
+        fz_im = z2_im + y;
+        mag = fz_re + fz_im;
+
+        i++;
     }
-    return k;
+
+    return i;
 }
 
 int main(int argc, char *argv[])
 {
-    int i, j; /* Iterate over pixels */
-    double xInterval = (XMAX - XMIN) / WIDTH;
-    double yInterval = (YMAX - YMIN) / HEIGHT;
-
-    for (i = 0; i < WIDTH / xInterval; i++)
-    {
-        for (j = 0; j < HEIGHT / yInterval; j++)
-        {
-            int s = mandelbrot(XMIN + i * xInterval, YMIN + j * yInterval);
-            if (s == MAX_ITERATIONS)
-            {
-                printf("*");
-            }
-        }
-    }
+    //int i, j;
 
     return EXIT_SUCCESS;
 }
