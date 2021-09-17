1d39b951b2316ff47bfa2b18790f33b302919f98
DONNY LEONE <leone.donny@gmail.com>
2021-09-17 21:19:49 +1000

    more concise implementation of mandelbrot()

--- mandelbrot.c
+++ mandelbrot.c
@@ -14,41 +14,46 @@ This program produces an image of the Mandelbrot set */
 #define YMIN -1
 #define YMAX 1
 
-/*  Escape time algorithm
-    Carries out the iteration z_k+1 = (z_k)^2 + c
+/*  This function is implements the escape time algorithm.
+    Calculates the iteration z_k+1 = (z_k)^2 + c
     u and v are Re(z_k) and Im(z_k) respectively
     x and y are Re(c) and Im(c) respectively */
 int mandelbrot(double u, double v, double x, double y)
 {
-    double z2_re = u * u - v * v; /* Calculate Re((z_k)^2) and Im((z_k)^2) */
-    double z2_im = 2 * u * v;
-    double fz_re = z2_re + x; /* Calculate Re(z_k+1) and Im(z_k+1) */
-    double fz_im = z2_im + y;
-    double mag = fz_re + fz_im; /* Magnitude squared of z_k+1 */
+    double z2_re;
+    double z2_im;
+    double fz_re;
+    double fz_im;
+    double mag = 0; /* Magnitude squared of z_k+1 */
     int i = 0;
 
     while (mag <= 4 && i < MAX_ITERATIONS)
     {
-        u = fz_re;
-        v = fz_im;
-        z2_re = u * u - v * v;
+        z2_re = u * u - v * v; /* Calculate Re((z_k)^2) and Im((z_k)^2) */
         z2_im = 2 * u * v;
-        fz_re = z2_re + x;
+        fz_re = z2_re + x; /* Calculate Re(z_k+1) and Im(z_k+1) */
         fz_im = z2_im + y;
         mag = fz_re + fz_im;
 
+        u = fz_re;
+        v = fz_im;
         i++;
     }
 
     return i;
 }
 
+void mandelbrotBitmap()
+{
+    /* Generate BMP image of Mandelbrot Set */
+}
+
 int main(int argc, char *argv[])
 {
     double i, j;
-    for (j = YMIN; j <= YMAX; j += 0.025)
+    for (j = YMIN; j <= YMAX; j += 0.0125)
     {
-        for (i = XMIN; i <= XMAX; i += 0.025)
+        for (i = XMIN; i <= XMAX; i += 0.0125)
         {
             if (mandelbrot(0, 0, i, j) == MAX_ITERATIONS)
             {
